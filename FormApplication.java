f0e9b59148bb7cb69bd47549225c06dffe1d920e
aoztepe07 <33508698+aoztepe07@users.noreply.github.com>
2017-11-09 09:31:15 +0300

    Create FormApplication.java

---
+++ FormApplication.java
@@ -0,0 +1,12 @@
+package com.example.formvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormvalidationApplication.class, args);
+	}
+}
